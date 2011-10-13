a963a32636d2f11f9caa419fce6444820b9a1b5b
Hannes Maier-Flaig <hamfbohne@gmail.com>
2011-10-13 15:54:20 +0200

    modify providing the page title to account for pages without title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,16 @@ module ApplicationHelper
       end
     else
       if @site == 'treffen' || @site == 'feste'
-        'veranstaltungen' 
+        'veranstaltungen'
+      else 
         @site
       end
     end
+  end
+
+  #def page_title= title <-- why can't I use this method name and then page_title = 'asdf' in the view?
+  def page_title title
+    #provide :title, title + ' ♣ ' <-- gives a syntax error only when using this special character
+    provide :title, title + ' KREUZ '
   end
 end
