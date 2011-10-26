02466a2adb77caa1d7782c47970e56d7cf929ab7
Hannes Maier-Flaig <hamfbohne@gmail.com>
2011-10-26 19:07:24 +0200

    explicitly stating :hash_data seems to solve the misterious uploaded-files-hashes-don't-match-issue

--- app/models/bewerbung.rb
+++ app/models/bewerbung.rb
@@ -13,14 +13,16 @@ class Bewerbung < ActiveRecord::Base
   has_attached_file :foto, {
     :styles => { :medium => "300x300>", :thumb => "100x100>" },
     :url => "/uploads/:hash.:extension",
-    :hash_secret => "712be566717dae4560fd7cfcf8214369"
+    :hash_secret => "712be566717dae4560fd7cfcf8214369",
+    :hash_data => ":class/:attachment/:id/:style"
   }
   validates_attachment_content_type :foto, :content_type => ['image/jpeg', 'image/png', 'image/gif']
   validates_attachment_size :foto, :less_than => 4.megabytes
 
   has_attached_file :lebenslauf, {
     :url => "/uploads/:hash.:extension",
-    :hash_secret => "6a3d3eebbf7356f1420e4359d3r"
+    :hash_secret => "6a3d3eebbf7356f1420e4359d3r",
+    :hash_data => ":class/:attachment/:id/:style"
   }
   validates_attachment_content_type :lebenslauf, :content_type => ['application/postscript',
                                                               'application/rtf',
