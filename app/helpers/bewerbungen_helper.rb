b969147bc9e880761bc74070c8235e951c9c3c99
Simon Wacker <simon.wacker@gmail.com>
2011-10-27 00:06:48 +0200

    use ruby properly

--- app/helpers/bewerbungen_helper.rb
+++ app/helpers/bewerbungen_helper.rb
@@ -6,9 +6,8 @@ module BewerbungenHelper
   end
 
   def errors_for_methods methods
-    html = ''
     if errors? methods
-      html << '<ol class="errors">'
+      html = '<ol class="errors">'
       methods.each do |method|
         html << error_message_on(@bewerbung, method.to_sym, :html_tag =>'li', :prepend_text => t("helpers.label.bewerbung.#{method}", :default => method.to_s.humanize) + " ")
       end
@@ -16,26 +15,22 @@ module BewerbungenHelper
       html.html_safe
     end
   end
-  
+
   def step_has_errors? step
-    has_error = false 
+    not errors_for_methods(methods_for_step step).blank?
+  end
+
+  def methods_for_step step
     case step
     when :persoenliches
-      has_error = true unless ( errors_for_methods(Bewerbung::PERSOENLICHE_ANGABEN).blank? \
-                    && errors_for_methods(Bewerbung::ANSCHRIFT_DER_ELTERN).blank? \
-                    && errors_for_methods(Bewerbung::WEITERE_KONTAKTINFORMATIONEN).blank? \
-                    && errors_for_methods(Bewerbung::ANGABEN_ZUM_STUDIUM).blank? )
+      Bewerbung::PERSOENLICHE_ANGABEN + Bewerbung::ANSCHRIFT_DER_ELTERN + Bewerbung::WEITERE_KONTAKTINFORMATIONEN + Bewerbung::ANGABEN_ZUM_STUDIUM
     when :organisatorisches
-      has_error = true unless ( errors_for_methods(Bewerbung::ANGABEN_ZUM_EINZUG).blank? \
-                    && errors_for_methods(Bewerbung::VORSTELLUNG).blank? \
-                    && errors_for_methods(Bewerbung::ORGANISATORISCHE_MITTEILUNGEN).blank? )
-    when :information
-      has_error = true unless ( errors_for_methods(Bewerbung::INFORMATIONEN).blank? )
+      Bewerbung::ANGABEN_ZUM_EINZUG + Bewerbung::VORSTELLUNG + Bewerbung::ORGANISATORISCHE_MITTEILUNGEN
+    when :informationen
+      Bewerbung::INFORMATIONEN
     end
-    
-    has_error
   end
-  
+
   def first_step_with_errors
     if step_has_errors? :persoenliches
       'persoenliches'
@@ -45,5 +40,4 @@ module BewerbungenHelper
       'motivation'
     end
   end
-      
 end
