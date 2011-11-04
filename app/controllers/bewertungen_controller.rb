eb9a51de9385e3cf6cc9e87b30fbd2734a20c617
Simon Wacker <simon.wacker@gmail.com>
2011-11-04 13:44:57 +0100

    bewertung was inserted twice: once with wert (when bewertung was saved) and once without (when bewerbung was saved in update_bewertung ... why?)

--- app/controllers/bewertungen_controller.rb
+++ app/controllers/bewertungen_controller.rb
@@ -2,11 +2,11 @@ class BewertungenController < ApplicationController
   before_filter :authenticate
 
   def create
-    @bewerbung = Bewerbung.find params[:bewerbung_id]
-    @bewertung = @bewerbung.bewertungen.find_or_initialize_by_benutzer :benutzer => @user
+    @bewertung = Bewertung.find_or_initialize_by_bewerbung_id_and_benutzer :bewerbung_id => params[:bewerbung_id], :benutzer => @user
     @bewertung.wert = params[:wert]
     @bewertung.save
 
+    @bewerbung = Bewerbung.find params[:bewerbung_id]
     @bewerbung.update_bewertung
 
     render :json => { :wert => @bewerbung.bewertung }
