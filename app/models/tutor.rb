71786d3d57b62642f5d9c8efac0b79009b1462a0
Simon Wacker <simon.wacker@gmail.com>
2012-05-13 20:53:49 +0200

    tutoriate

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,4 +1,10 @@
 class Tutor < ActiveRecord::Base
   establish_connection :hekdb
   set_table_name 'tutorenliste'
+
+  def self.anzahl
+    all.map do |tutor|
+      "#{tutor.vorname} #{tutor.zimmernummer}"
+    end.uniq.count
+  end
 end
