16740ebf16a7c9a0a7309ae21d554a3de46091e4
philip <philip@philip-E6224.(none)>
2013-10-21 09:09:18 +0200

    added some lines concerning error_notification, hopefully it will work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,5 +67,13 @@ Hek::Application.configure do
     :exception_recipients => %w{ hekweb@hek.uni-karlsruhe.de },
     :ignore_crawlers => %w{ Googlebot bingbot }
 
+  config.middleware.use ExceptionNotification::Rack,
+    :email => {
+    :email_prefix => "[HEK ExceptionNotification] ",
+    :sender_address => %{ "HEK Exception" <exception@hek.uni-karlsruhe.de> },
+    :exception_recipients => %w{ hekweb@hek.uni-karlsruhe.de },
+    :ignore_crawlers => %w{ Googlebot bingbot }
+  }
+
   Paperclip.options[:command_path] = '/usr/bin/'
 end
